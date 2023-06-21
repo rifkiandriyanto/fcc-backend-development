d09ea5f487c86fc992a4ec04648f5bc87faa84a7
Rifki Andriyanto <andriyantorifki@gmail.com>
2023-06-21 10:33:19 +0700

    feat: add solution

--- timestamp/index.js
+++ timestamp/index.js
@@ -8,23 +8,43 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// API endpoint for handling date requests
+app.get('/api/:date?', function (req, res) {
+  // Retrieve the date parameter from the request
+  var dateString = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // Check if the date parameter is empty
+  if (!dateString) {
+    // If the date parameter is empty, return the current time
+    var currentDate = new Date();
+    return res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString()
+    });
+  }
+
+  // Parse the date string and handle any invalid dates
+  var date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  // Return the JSON object with the unix and utc keys
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
