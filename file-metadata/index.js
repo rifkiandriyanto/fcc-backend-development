39c2c383a1ff05553a56f8ca93764f15629d9e67
Rifki Andriyanto <andriyantorifki@gmail.com>
2023-06-21 14:28:36 +0700

    feat: add file-metadata project

---
+++ file-metadata/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(JSON.stringify(Object.keys(req.file)));
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Node.js listening ...');
+});
